0d3231fe7f5f2822af55a481b8223d21cec501fd
罗树新 <luoshuxin@renrenche.com>
2021-03-03 10:39:45 +0800

    MOD: 增加默认unknown

--- Sources/SmartCodable/SmartDecoder.swift
+++ Sources/SmartCodable/SmartDecoder.swift
@@ -78,3 +78,12 @@ public extension Decoder {
         return date
     }
 }
+
+protocol CaseIterableDefaultsLast: SmartDecodable & CaseIterable & RawRepresentable
+where RawValue: Decodable, AllCases: BidirectionalCollection { }
+
+extension CaseIterableDefaultsLast {
+    init(from decoder: Decoder) throws {
+        self = try Self(rawValue: decoder.singleValueContainer().decode(RawValue.self)) ?? Self.allCases.last!
+    }
+}
